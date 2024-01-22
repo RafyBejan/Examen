0061d814cf5307233e933ed48c43275bc3aae877
bejan <bejan@RAFAEL12>
2024-01-22 10:10:29 +0200

    initial commit

---
+++ ExaemenOop/ExaemenOop/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExaemenOop
+{
+    internal class Person
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public DateTime YearOfBirth { get; set; }
+        public int Age
+        {
+            get
+            {
+                return DateTime.Now.Year - YearOfBirth.Year;
+            }
+        }
+    }
+}
